db9bf16d8c2f0b6246b08a68914277cc3e2e0535
Abrham Sertse <abrishsertse@gmail.com>
2023-02-21 11:16:09 -0800

    before advanced

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,17 +1,14 @@
-#include "main.h"		
-		
+#include "main.h"
+
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
-		
 int main(void)
-		
 {
-		
     int r;
-		
+
     r = _islower('H');
     _putchar(r + '0');
     r = _islower('o');
